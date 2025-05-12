766f7555c47859a414a95619b21486ffa8830b9b
DK4REAL <91735459+DK4REAL@users.noreply.github.com>
2025-05-12 15:47:20 +0200

    Events created

--- data/events.js
+++ data/events.js
@@ -16,6 +16,60 @@ const events = [
     date: '2025-06-15',
     location: 'Johannesburg',
     image: 'hackathon.jpg'
+  },
+  {
+    title: 'Meet and Greet',
+    date: '2025-08-14',
+    location: 'Pretoria',
+    image: 'meetgreet.png'
+  },
+  {
+    title: 'Cyber security Talk',
+    date: '2025-07-19',
+    location: 'Port Elizabeth',
+    image: 'cybersec.png'
+  },
+  {
+    title: 'Cyberbullying',
+    date: '2025-10-01',
+    location: 'Online',
+    image: 'cybBully.png'
+  },
+  {
+    title: 'Password Safety',
+    date: '2025-07-21',
+    location: 'Durban',
+    image: 'passwordSafe.png'
+  },
+  {
+    title: 'Typing Class',
+    date: '2025-11-19',
+    location: 'Online',
+    image: 'typeClass.png'
+  },
+  {
+    title: 'Introduction to Programming',
+    date: '2026-02-04',
+    location: 'Upington',
+    image: 'intProg.png'
+  },
+  {
+    title: 'Graduation',
+    date: '2026-04-23',
+    location: 'Pretoria',
+    image: 'graduate.png'
+  },
+  {
+    title: 'LAN Contest',
+    date: '2025-08-29',
+    location: 'Pretoria',
+    image: 'LAN.png'
+  },
+  {
+    title: 'Team Building ',
+    date: '2025-11-01',
+    location: 'Cape Town',
+    image: 'teamBuild.png'
   }
 ];
 
